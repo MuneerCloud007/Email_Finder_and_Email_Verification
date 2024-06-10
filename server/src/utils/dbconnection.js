98c3945acd35ea2e3ccb4e275f47a316c63d12e0
MuneerCloud007 <CloudVandana@007>
2024-06-10 18:32:15 +0530

    code is commited-3

--- server/src/utils/dbconnection.js
+++ server/src/utils/dbconnection.js
@@ -1,5 +1,5 @@
 import mongoose from 'mongoose';
-const db = process.env.mongoDB_url || "mongodb+srv://cloudvandana:cloudvandana@cluster0.kwfrpcs.mongodb.net/Email_Finder_and_Email_Verification?retryWrites=true&w=majority&appName=Cluster0PORT=6000";
+const db = "mongodb+srv://cloudvandana:cloudvandana@cluster0.kwfrpcs.mongodb.net/Email_Finder_and_Email_Verification?retryWrites=true&w=majority&appName=Cluster0PORT=6000";
 
 const connectDB = async () => {
     try {
