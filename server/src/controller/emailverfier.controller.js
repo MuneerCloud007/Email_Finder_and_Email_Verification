e5e06a072bb38de971a16862ec5683f58ef0e579
MuneerCloud007 <CloudVandana@007>
2024-06-12 17:40:04 +0530

    added two server

--- server/src/controller/emailverfier.controller.js
+++ server/src/controller/emailverfier.controller.js
@@ -3,10 +3,10 @@ import  WebscrapingData  from '../webscraping/webscrap.js';
 import ApiError from '../utils/ApiError.js';
 import {URL} from 'url';
 import wrapperFunEmailVerfier from "../email_verfication/emailVerfier.algo.js";
+import axios from "axios"
 
 const scrapeController = async (req, res, next) => {
     const { url,firstName,lastName } = req.body;
-    console.log("I am here =" + url);
 
     if (!url) {
         return next(ApiError.badRequest('URL is required'));
@@ -14,10 +14,13 @@ const scrapeController = async (req, res, next) => {
 
     try {
         console.log(url);
-        const companyLink = await WebscrapingData(url);
 
-        const urlString = companyLink;
-        const parsedUrl = new URL(urlString.Link);
+        const urlString =await axios.post("https://puputeer-render.onrender.com/scrape",{
+            "url":url
+          
+        });
+        console.log(urlString.data.Link);
+        const parsedUrl = new URL(urlString.data.Link);
 
         // Extract domain
         const domain = parsedUrl.hostname.replace(/^www\./, ''); // Remove 'www.' if present
